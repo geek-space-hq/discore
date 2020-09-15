8fd4aa48b7393a350a56cdbdbad220617e3f9fb0
sh1ma <in9lude@gmail.com>
2020-09-15 16:12:59 +0900

    feat: channel model

--- discore/models/_channel.py
+++ discore/models/_channel.py
@@ -1,9 +1,46 @@
-from dataclasses import dataclass, field
-from typing import List, Optional
+from datetime import datetime
+from typing import List, Optional, Union
+
+from pydantic import BaseModel
+from pydantic.fields import Field
+
+# pylint: disable=unused-import
+from ._user import User
 
 
-@dataclass
-class Channel:
+class Channel(BaseModel):
     id: str
     type: int
     guild_id: Optional[str] = None
+    position: Optional[int] = None
+    permission_overwrites: Optional["Overwrite"] = None
+    name: Optional[str] = None
+    topic: Optional[str] = None
+    nsfw: bool = Field(default=False)
+    last_message_id: Optional[str] = None
+    bitrate: Optional[int] = None
+    user_limit: Optional[int] = None
+    rate_limit_per_user: Optional[int] = None
+    recipients: Optional[List["User"]] = None
+    icon: Optional[str] = None
+    parent_id: Optional[str] = None
+    last_pin_timestamp: Optional[datetime] = None
+
+
+class OverwriteReceiving(BaseModel):
+    id: str
+    type: str
+    allow: int
+    allow_new: str
+    deny: int
+    deny_new: str
+
+
+class OverwriteSending(BaseModel):
+    id: str
+    type: str
+    allow: Union[int, str]
+    deny: Union[int, str]
+
+
+Overwrite = Union[OverwriteReceiving, OverwriteSending]
